5d3e165d6a97a339be48025f57434922fbc691ed
Sacrimento <sacrimentocorp@gmail.com>
2020-11-11 23:07:00 +0100

    makefiles + open sully

--- Sully/asm/Sully.s
+++ Sully/asm/Sully.s
@@ -9,8 +9,8 @@ section .bss
 section .data
 	X: dq 5
 	fmt_path: db "Sully_%d.s", 0
-	fmt_cmd: db "nasm -f elf64 -o Sully_%d.o Sully_%1$d.s; gcc -o Sully_%1$d Sully_%1$d.o; ./Sully_%1$d", 0
-	s: db "extern sprintf%1$cextern dprintf%1$cextern system%1$c%1$csection .bss%1$c%2$cpath: resb 4096%1$c%2$ccmd: resb 4096%1$c%1$csection .data%1$c%2$cX: dq %4$d%1$c%2$cfmt_path: db %3$cSully_%%d.s%3$c, 0%1$c%2$cfmt_cmd: db %3$cnasm -f elf64 -o Sully_%%d.o Sully_%%1$d.s; gcc -o Sully_%%1$d Sully_%%1$d.o; ./Sully_%%1$d%3$c, 0%1$c%2$cs: db %3$c%5$s%3$c, 0%1$c%1$csection .text%1$c%2$cglobal main%1$c%1$cmain:%1$c%2$cmov r12, [X]%1$c%2$ccmp r12, 0%1$c%2$cjle no_error%1$c%2$ccmp r12, 4000%1$c%2$cjg error%1$c%2$cdec r12%1$c%1$c%2$clea rdi, [path]%1$c%2$clea rsi, [fmt_path]%1$c%2$cmov rdx, r12%1$c%2$cmov al, 0%1$c%2$ccall sprintf%1$c%1$c%2$cmov rax, 2%1$c%2$clea rdi, [path]%1$c%2$cmov rsi, 65%1$c%2$cmov rdx, 0644o%1$c%2$csyscall%1$c%2$ccmp rax, 0%1$c%2$cjle error%1$c%2$cmov r13, rax%1$c%1$c%2$cmov rdi, rax%1$c%2$clea rsi, [s]%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, r12%1$c%2$cpush dword s%1$c%2$cmov al, 0%1$c%2$ccall dprintf%1$c%1$c%2$cmov rax, 3%1$c%2$cmov rdi, r13%1$c%2$csyscall%1$c%1$c%2$clea rdi, [cmd]%1$c%2$clea rsi, [fmt_cmd]%1$c%2$cmov rdx, r12%1$c%2$cmov al, 0%1$c%2$ccall sprintf%1$c%1$c%2$clea rdi, [cmd]%1$c%2$ccall system%1$c%1$c%2$cjmp no_error%1$c%1$cerror:%1$c%2$cpush 1%1$c%2$cjmp exit%1$c%1$cno_error:%1$c%2$cpush 0%1$c%2$cjmp exit%1$c%1$cexit:%1$c%2$cmov rax, 60%1$c%2$cpop rdi%1$c%2$csyscall%1$c", 0
+	fmt_cmd: db "nasm -f elf64 -o Sully_%d.o Sully_%1$d.s; clang -o Sully_%1$d Sully_%1$d.o; ./Sully_%1$d", 0
+	s: db "extern sprintf%1$cextern dprintf%1$cextern system%1$c%1$csection .bss%1$c%2$cpath: resb 4096%1$c%2$ccmd: resb 4096%1$c%1$csection .data%1$c%2$cX: dq %4$d%1$c%2$cfmt_path: db %3$cSully_%%d.s%3$c, 0%1$c%2$cfmt_cmd: db %3$cnasm -f elf64 -o Sully_%%d.o Sully_%%1$d.s; clang -o Sully_%%1$d Sully_%%1$d.o; ./Sully_%%1$d%3$c, 0%1$c%2$cs: db %3$c%5$s%3$c, 0%1$c%1$csection .text%1$c%2$cglobal main%1$c%1$cmain:%1$c%2$cmov r12, [X]%1$c%2$ccmp r12, 0%1$c%2$cjl no_error%1$c%2$ccmp r12, 4096%1$c%2$cjg error%1$c%2$cmov r14, r12%1$c%2$cdec r12%1$c%1$c%2$clea rdi, [path]%1$c%2$clea rsi, [fmt_path]%1$c%2$cmov rdx, r14%1$c%2$cmov al, 0%1$c%2$ccall sprintf%1$c%1$c%2$cmov rax, 2%1$c%2$clea rdi, [path]%1$c%2$cmov rsi, 65%1$c%2$cmov rdx, 0644o%1$c%2$csyscall%1$c%2$ccmp rax, 0%1$c%2$cjle error%1$c%2$cmov r13, rax%1$c%1$c%2$cmov rdi, rax%1$c%2$clea rsi, [s]%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, r12%1$c%2$cpush dword s%1$c%2$cmov al, 0%1$c%2$ccall dprintf%1$c%1$c%2$cmov rax, 3%1$c%2$cmov rdi, r13%1$c%2$csyscall%1$c%1$c%2$ccmp r14, 0%1$c%2$cjle no_error%1$c%1$c%2$clea rdi, [cmd]%1$c%2$clea rsi, [fmt_cmd]%1$c%2$cmov rdx, r14%1$c%2$cmov al, 0%1$c%2$ccall sprintf%1$c%1$c%2$clea rdi, [cmd]%1$c%2$ccall system%1$c%1$c%2$cjmp no_error%1$c%1$cerror:%1$c%2$cpush 1%1$c%2$cjmp exit%1$c%1$cno_error:%1$c%2$cpush 0%1$c%2$cjmp exit%1$c%1$cexit:%1$c%2$cmov rax, 60%1$c%2$cpop rdi%1$c%2$csyscall%1$c", 0
 
 section .text
 	global main
@@ -18,14 +18,15 @@ section .text
 main:
 	mov r12, [X]
 	cmp r12, 0
-	jle no_error
-	cmp r12, 4000
+	jl no_error
+	cmp r12, 4096
 	jg error
+	mov r14, r12
 	dec r12
 
 	lea rdi, [path]
 	lea rsi, [fmt_path]
-	mov rdx, r12
+	mov rdx, r14
 	mov al, 0
 	call sprintf
 
@@ -52,9 +53,12 @@ main:
 	mov rdi, r13
 	syscall
 
+	cmp r14, 0
+	jle no_error
+
 	lea rdi, [cmd]
 	lea rsi, [fmt_cmd]
-	mov rdx, r12
+	mov rdx, r14
 	mov al, 0
 	call sprintf
 
--- Sully/python/Sully.py
+++ Sully/python/Sully.py
@@ -1,12 +1,17 @@
 import os
 X = 5
 def main():
-	if X <= 0:
+	if X < 0:
 		return
-	s = "import os{0}X = {4}{0}def main():{0}{1}if X <= 0:{0}{1}{1}return{0}{1}s = {2}{3}{2}{0}{1}f = open({2}Sully_{5}{6}.py{2}.format(X-1), {2}w{2}){0}{1}f.write(s.format(chr(10), chr(9), chr(34), s, X-1, chr(123), chr(125))){0}{1}f.close(){0}{1}os.system({2}python Sully_{5}{6}.py{2}.format(X-1)){0}{0}main(){0}"
-	f = open("Sully_{}.py".format(X-1), "w")
+	s = "import os{0}X = {4}{0}def main():{0}{1}if X < 0:{0}{1}{1}return{0}{1}s = {2}{3}{2}{0}{1}try:{0}{1}{1}f = open({2}Sully_{5}{6}.py{2}.format(X), {2}w{2}){0}{1}except OSError:{0}{1}{1}return{0}{1}f.write(s.format(chr(10), chr(9), chr(34), s, X-1, chr(123), chr(125))){0}{1}f.close(){0}{1}if X <= 0:{0}{1}{1}return{0}{1}os.system({2}python Sully_{5}{6}.py{2}.format(X)){0}{0}main(){0}"
+	try:
+		f = open("Sully_{}.py".format(X), "w")
+	except OSError:
+		return
 	f.write(s.format(chr(10), chr(9), chr(34), s, X-1, chr(123), chr(125)))
 	f.close()
-	os.system("python Sully_{}.py".format(X-1))
+	if X <= 0:
+		return
+	os.system("python Sully_{}.py".format(X))
 
 main()
